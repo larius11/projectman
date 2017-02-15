dacb819746c90d78b5d32d2e1d6edc56be50cb19
Ricardo Riveron <larius828@utexas.edu>
2017-02-14 21:09:38 -0600

    ...

---
+++ calendar/dates.php
@@ -0,0 +1,13 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "This example event is different.",
+			"url": "http://www.example.com/",
+			"class": "event-warning",
+			"start": "1486703830740",
+			"end":   "1487128139664"
+		}
+	]
+}
