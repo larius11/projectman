2ecbe9be3051da82bf351996062be630f6534b95
Ricardo Riveron <larius828@utexas.edu>
2017-02-16 20:42:52 -0600

    Adding a class dropdown menu

--- php/add_process.php
+++ php/add_process.php
@@ -1,40 +1,47 @@
 <?php 
 
-	// define('DB_NAME','service');
-	// define('DB_USER','heb');
-	// define('DB_PASSWORD','Austin04');
-	// define('DB_HOST', 'localhost');
+	define('DB_NAME','service');
+	define('DB_USER','heb');
+	define('DB_PASSWORD','Austin04');
+	define('DB_HOST', 'localhost');
 
-	// $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
-	// if (!$link) {
-	// 	die('Could not connect: ' . mysql_error());
-	// }
+	if (!$link) {
+		die('Could not connect: ' . mysql_error());
+	}
 
-	// $db_selected = mysql_select_db(DB_NAME, $link);
+	$db_selected = mysql_select_db(DB_NAME, $link);
 
-	// if (!$db_selected){
-	// 	die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
-	// }
+	if (!$db_selected){
+		die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+	}
 
 	$title = $_POST['title'];
+	$url = "#";
+	$class = $_POST['class'];
 	$start = $_POST['start'];
 	$end = $_POST['end'];
 
 	echo($title);
-	echo(" 1\n");
+	echo(" 1<br>");
+	echo($url);
+	echo(" 2<br>");
+	echo($class);
+	echo(" 3<br>");	
 	echo($start);
-	echo(" 2\n");
+	echo(" 4<br>");
 	echo($end);
-	echo(" 2\n");
-	
-	
-	
+	echo(" 5<br>");
 
-	// $col = "title, body";
-	// $val = "'$title', '$body'";
+	// if ($start == $end){
+	// 	$end += 28800000;
+	// }
 
-	// $sql = "INSERT INTO projects ($col) VALUES ($val)";
+	// $col = "ID, title, url, class, start, end";
+	// $val = "NULL, '$title', '$url', '$class', '$start', '$end'";
+
+	// $sql = "INSERT INTO  events_list ($col) VALUES ($val)";
 
 	// if (!mysql_query($sql)){
 	// 	die('Error: ' . mysql_error());
