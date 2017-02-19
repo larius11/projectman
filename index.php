c651c4259cf58f89793dbd4fdc0ab5cdde8c7cdb
Ricardo Riveron <larius828@utexas.edu>
2017-02-19 01:30:25 -0600

    ...

--- index.php
+++ index.php
@@ -137,30 +137,10 @@
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
-                    <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="tasks.html"><i class="fa fa-fw fa-bar-chart-o"></i> Tasks</a>
-                    </li>
-                    <li>
-                        <a href="communication.html"><i class="fa fa-fw fa-desktop"></i> Communication </a>
-                    </li>
-                    <li>
-                        <a href="calendar.html"><i class="fa fa-fw fa-calendar"></i>
-                        Calendar </a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Account <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="demo" class="collapse">
-                            <li>
-                                <a href="#">Messages</a>
-                            </li>
-                            <li>
-                                <a href="#">Embedded Chat</a>
-                            </li>
-                        </ul>
-                    </li>
+                    <?php
+                        $active = "Dashboard";
+                        include 'php/nav.php';
+                    ?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
---
+++ php/nav.php
@@ -0,0 +1,25 @@
+<?php
+    echo "<li class=\"active\">
+        <a href=\"index.html\"><i class=\"fa fa-fw fa-dashboard\"></i> Dashboard</a>
+    </li>
+    <li>
+        <a href=\"tasks.html\"><i class=\"fa fa-fw fa-bar-chart-o\"></i> Tasks</a>
+    </li>
+    <li>
+        <a href=\"communication.html\"><i class=\"fa fa-fw fa-desktop\"></i> Communication </a>
+    </li>
+    <li>
+        <a href=\"calendar.html\"><i class=\"fa fa-fw fa-calendar\"></i>Calendar </a>
+    </li>
+    <li>
+        <a href=\"javascript:;\" data-toggle=\"collapse\" data-target=\"#demo\"><i class=\"fa fa-fw fa-arrows-v\"></i> Account <i class=\"fa fa-fw fa-caret-down\"></i></a>
+        <ul id=\"demo\" class=\"collapse\">
+            <li>
+                <a href=\"#\">Messages</a>
+            </li>
+            <li>
+                <a href=\"#\">Embedded Chat</a>
+            </li>
+        </ul>
+    </li>";
+?>
