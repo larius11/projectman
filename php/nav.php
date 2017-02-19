7b0e57e8904c66c730909dbb0fbbdfce31f586ed
Ricardo Riveron <larius828@utexas.edu>
2017-02-19 02:08:24 -0600

    ...

--- php/nav.php
+++ php/nav.php
@@ -16,6 +16,9 @@
                     <a href=\"calendar.php\"><i class=\"fa fa-fw fa-calendar\"></i>Calendar </a>
                 </li>
                 <li>
+                    <a href=\"faq.php\"><i class=\"fa fa-fw fa-book\"></i>Pam's Notebook</a>
+                </li>
+                <li>
                     <a href=\"javascript:;\" data-toggle=\"collapse\" data-target=\"#demo\"><i class=\"fa fa-fw fa-arrows-v\"></i> Account <i class=\"fa fa-fw fa-caret-down\"></i></a>
                     <ul id=\"demo\" class=\"collapse\">
                         <li>
@@ -39,6 +42,9 @@
                 </li>
                 <li>
                     <a href=\"calendar.php\"><i class=\"fa fa-fw fa-calendar\"></i>Calendar </a>
+                </li>
+                <li>
+                    <a href=\"faq.php\"><i class=\"fa fa-fw fa-book\"></i>Pam's Notebook</a>
                 </li>
                 <li>
                     <a href=\"javascript:;\" data-toggle=\"collapse\" data-target=\"#demo\"><i class=\"fa fa-fw fa-arrows-v\"></i> Account <i class=\"fa fa-fw fa-caret-down\"></i></a>
@@ -66,6 +72,9 @@
                     <a href=\"calendar.php\"><i class=\"fa fa-fw fa-calendar\"></i>Calendar </a>
                 </li>
                 <li>
+                    <a href=\"faq.php\"><i class=\"fa fa-fw fa-book\"></i>Pam's Notebook</a>
+                </li>
+                <li>
                     <a href=\"javascript:;\" data-toggle=\"collapse\" data-target=\"#demo\"><i class=\"fa fa-fw fa-arrows-v\"></i> Account <i class=\"fa fa-fw fa-caret-down\"></i></a>
                     <ul id=\"demo\" class=\"collapse\">
                         <li>
@@ -79,6 +88,34 @@
             break;
         case "calendar":
            echo "<li>
+                    <a href=\"http://www.ricardoriveron.com/projectman/index.php\"><i class=\"fa fa-fw fa-dashboard\"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href=\"http://www.ricardoriveron.com/projectman/tasks.php\"><i class=\"fa fa-fw fa-bar-chart-o\"></i> Tasks</a>
+                </li>
+                <li>
+                    <a href=\"http://www.ricardoriveron.com/projectman/communication.php\"><i class=\"fa fa-fw fa-desktop\"></i> Communication </a>
+                </li>
+                <li class=\"active\">
+                    <a href=\"http://www.ricardoriveron.com/projectman/calendar.php\"><i class=\"fa fa-fw fa-calendar\"></i>Calendar </a>
+                </li>
+                <li>
+                    <a href=\"http://www.ricardoriveron.com/projectman/faq.php\"><i class=\"fa fa-fw fa-book\"></i>Pam's Notebook</a>
+                </li>
+                <li>
+                    <a href=\"javascript:;\" data-toggle=\"collapse\" data-target=\"#demo\"><i class=\"fa fa-fw fa-arrows-v\"></i> Account <i class=\"fa fa-fw fa-caret-down\"></i></a>
+                    <ul id=\"demo\" class=\"collapse\">
+                        <li>
+                                <a href=\"#\">Messages</a>
+                        </li>
+                        <li>
+                                <a href=\"#\">Embedded Chat</a>
+                        </li>
+                    </ul>
+                </li>";
+            break;
+        case "pam":
+           echo "<li>
                     <a href=\"index.php\"><i class=\"fa fa-fw fa-dashboard\"></i> Dashboard</a>
                 </li>
                 <li>
@@ -87,8 +124,11 @@
                 <li>
                     <a href=\"communication.php\"><i class=\"fa fa-fw fa-desktop\"></i> Communication </a>
                 </li>
-                <li class=\"active\">
+                <li>
                     <a href=\"calendar.php\"><i class=\"fa fa-fw fa-calendar\"></i>Calendar </a>
+                </li>
+                <li class=\"active\">
+                    <a href=\"faq.php\"><i class=\"fa fa-fw fa-book\"></i>Pam's Notebook</a>
                 </li>
                 <li>
                     <a href=\"javascript:;\" data-toggle=\"collapse\" data-target=\"#demo\"><i class=\"fa fa-fw fa-arrows-v\"></i> Account <i class=\"fa fa-fw fa-caret-down\"></i></a>
