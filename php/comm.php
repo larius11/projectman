750b2debc49a30b67a394639fc28ec4581c4162c
Ricardo Riveron <larius828@utexas.edu>
2017-02-20 01:03:07 -0600

    ....

--- php/comm.php
+++ php/comm.php
@@ -27,54 +27,54 @@
 	$i = 0;
 	$more = true;
 	echo "<p>Im about to end.</p>";
-	// while($more) {
-	// 	$data = mysql_fetch_row($result);
+	while($more) {
+		$data = mysql_fetch_row($result);
 
-	// 	if (!($data)){
-	// 		$more = false;
-	// 	}else{
-	// 		$elapsed = (strtotime($today)-strtotime($data[2]))/60;
-	// 		if ($i%2 == 0){
-	// 			$post[$i] = "
-	// 			<li>
- //                    <div class=\"timeline-badge\">
- //                        <i class=\"fa fa-user\" aria-hidden=\"true\"></i>
- //                    </div>
- //                    <div class=\"timeline-user\">
- //                            ". $data[1] . "
- //                    </div>
- //                    <div class=\"timeline-panel\">
- //                        <div class=\"timeline-heading\">
- //                            <h4 class=\"timeline-title\">".$data[3]."</h4>
- //                            <p><small class=\"text-muted\"><i class=\"glyphicon glyphicon-time\"></i>".($elapsed)." minutes ago</small></p>
- //                        </div>
- //                        <div class=\"timeline-body\">
- //                            <p>".$data[4]."</p>
- //                        </div>
- //                    </div>
- //                </li>"
-	// 		}else{
-	// 			$post[$i] = "
-	// 			<li clas=\"timeline-inverted\">
- //                    <div class=\"timeline-badge\">
- //                        <i class=\"fa fa-user\" aria-hidden=\"true\"></i>
- //                    </div>
- //                    <div class=\"timeline-useri\">
- //                            ". $data[1] . "
- //                    </div>
- //                    <div class=\"timeline-panel\">
- //                        <div class=\"timeline-heading\">
- //                            <h4 class=\"timeline-title\">".$data[3]."</h4>
- //                            <p><small class=\"text-muted\"><i class=\"glyphicon glyphicon-time\"></i>".($elapsed)." minutes ago</small></p>
- //                        </div>
- //                        <div class=\"timeline-body\">
- //                            <p>".$data[4]."</p>
- //                        </div>
- //                    </div>
- //                </li>"
-	// 		}
-	// 		echo $post[$i];
-	// 		$i++;
-	// 	}
-	// }
+		if (!($data)){
+			$more = false;
+		}else{
+			$elapsed = (strtotime($today)-strtotime($data[2]))/60;
+			if ($i%2 == 0){
+				$post[$i] = "
+				<li>
+                    <div class=\"timeline-badge\">
+                        <i class=\"fa fa-user\" aria-hidden=\"true\"></i>
+                    </div>
+                    <div class=\"timeline-user\">
+                            ". $data[1] . "
+                    </div>
+                    <div class=\"timeline-panel\">
+                        <div class=\"timeline-heading\">
+                            <h4 class=\"timeline-title\">".$data[3]."</h4>
+                            <p><small class=\"text-muted\"><i class=\"glyphicon glyphicon-time\"></i>".($elapsed)." minutes ago</small></p>
+                        </div>
+                        <div class=\"timeline-body\">
+                            <p>".$data[4]."</p>
+                        </div>
+                    </div>
+                </li>"
+			}else{
+				$post[$i] = "
+				<li clas=\"timeline-inverted\">
+                    <div class=\"timeline-badge\">
+                        <i class=\"fa fa-user\" aria-hidden=\"true\"></i>
+                    </div>
+                    <div class=\"timeline-useri\">
+                            ". $data[1] . "
+                    </div>
+                    <div class=\"timeline-panel\">
+                        <div class=\"timeline-heading\">
+                            <h4 class=\"timeline-title\">".$data[3]."</h4>
+                            <p><small class=\"text-muted\"><i class=\"glyphicon glyphicon-time\"></i>".($elapsed)." minutes ago</small></p>
+                        </div>
+                        <div class=\"timeline-body\">
+                            <p>".$data[4]."</p>
+                        </div>
+                    </div>
+                </li>"
+			}
+			echo $post[$i];
+			$i++;
+		}
+	}
  ?>
