3ab054bd541f788eca1778e1713067f6cff2f667
Ricardo Riveron <larius828@utexas.edu>
2017-02-20 03:02:53 -0600

    ...

--- php/login.php
+++ php/login.php
@@ -34,62 +34,59 @@
 			echo 'Login Failed; <br> Try Again.';
 		}
 	} else {
-?>
-<html lang="en">
+echo "<html lang=\"en\">
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta charset=\"utf-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
 
     <title>Service Admin Website</title>
 
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/login.css" rel="stylesheet">
+    <link href=\"../css/bootstrap.min.css\" rel=\"stylesheet\">
+    <link href=\"../css/login.css\" rel=\"stylesheet\">
 
-    <link href="../css/sb-admin.css" rel="stylesheet">
+    <link href=\"../css/sb-admin.css\" rel=\"stylesheet\">
 
-    <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href=\"../font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\">
 
 </head>
 <body>
-	<div id="wrapper">
-		<div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                	<div class="col-lg-3 col-md-3">
+	<div id=\"wrapper\">
+		<div id=\"page-wrapper\">
+            <div class=\"container-fluid\">
+                <div class=\"row\">
+                	<div class=\"col-lg-3 col-md-3\">
                 	</div>
-                	<div class="col-lg-6 col-md-6">
-                		<a href="#" data-toggle="modal" data-target="#login-modal">Login</a>
+                	<div class=\"col-lg-6 col-md-6\">
+                		<a href=\"#\" data-toggle=\"modal\" data-target=\"#login-modal\">Login</a>
 
-						<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-						    	  <div class="modal-dialog">
-										<div class="loginmodal-container">
+						<div class=\"modal fade\" id=\"login-modal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\" style=\"display: none;\">
+						    	  <div class=\"modal-dialog\">
+										<div class=\"loginmodal-container\">
 											<h1>Login to Your Account</h1><br>
 										  <form>
-											<input type="text" name="user" placeholder="Username">
-											<input type="password" name="pass" placeholder="Password">
-											<input type="submit" name="login" class="login loginmodal-submit" value="Login">
+											<input type=\"text\" name=\"user\" placeholder=\"Username\">
+											<input type=\"password\" name=\"pass\" placeholder=\"Password\">
+											<input type=\"submit\" name=\"login\" class=\"login loginmodal-submit\" value=\"Login\">
 										  </form>
 											
-										  <div class="login-help">
-											<a href="#">Register</a> - <a href="#">Forgot Password</a>
+										  <div class=\"login-help\">
+											<a href=\"#\">Register</a> - <a href=\"#\">Forgot Password</a>
 										  </div>
 										</div>
 									</div>
 								  </div>
                 	</div>
-                	<div class="col-lg-3 col-md-3">
+                	<div class=\"col-lg-3 col-md-3\">
                 	</div>
                 </div>
             </div>
         </div>
 	</div>
 </body>
-</html>
-
-<?php
+</html>";
 }
 ?>
