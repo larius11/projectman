c284e248afe475a4c4ff7ef1cf6e3d24ac913adb
Ricardo Riveron <larius828@utexas.edu>
2017-02-20 04:06:04 -0600

    ...

--- php/login.php
+++ php/login.php
@@ -19,19 +19,23 @@
 			die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
 		}
 
-		$username = mysql_real_escape_string($_POST['user']);
-        $password = sha1($_POST['pass'] );
+		// Get values passed from the login-form in login.php file
+		$username = $_POST['user'];
+		$password = $_POST['pass'];
 
-        $query = mysql_query("SELECT * FROM users WHERE user='".addSlashes($username)."' AND pass='".addSlashes($password)."'");
-        $res = mysql_num_rows($query);
+		// Still need to prevent mysql injection, need check the input eventually
 
-        if ($res == 1) {
-            $_SESSION['username'] = $username;
-            $_SESSION['password'] = $password;
-            $_SESSION['userobj'] = mysql_fetch_assoc($query);
+		// connect to the server's database
+		mysql_connect("localhost", "root", "richiboy1");
+		mysql_select_db("login");
 
-            header('Location: ../index.php');
-            exit;
+		// Query the database for user
+		$result = mysql_query("select * from users where user = '$username' and pass = '$password'") or die("Failed to query database ".mysql_error());
+		$row = mysql_fetch_array($result);
+
+        if (($row['user'] == $username) && ($row['pass'] == $password) ){
+			header("Location: ../index.php");
+			exit;
         } else {
             echo 'Data does not match <br /> RE-Enter Username and Password';
             header('Location: login.php');
