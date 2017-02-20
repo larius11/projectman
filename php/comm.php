6fceee55336c3631799ddbf2fa6ffbceabf76cbc
Ricardo Riveron <larius828@utexas.edu>
2017-02-20 01:35:17 -0600

    ...

--- php/comm.php
+++ php/comm.php
@@ -37,7 +37,7 @@
 			echo ($elapsed);
 			echo "<br>";
 			if ($i%2 == 0){
-				$post[$i] = "<li><div class=\"timeline-badge\"><i class=\"fa fa-user\" aria-hidden=\"true\"></i></div><div class=\"timeline-user\">$data[1]</div><div class=\"timeline-panel\"><div class=\"timeline-heading\"><h4 class=\"timeline-title\">$data[3]</h4><p><small class=\"text-muted\"><i class=\"glyphicon glyphicon-time\"></i>$elapsed minutes ago</small></p></div><div class=\"timeline-body\"><p>$data[4]</p></div></div></li>"
+				$post[$i] = "<li><div class=\"timeline-badge\"><i class=\"fa fa-user\" aria-hidden=\"true\"></i></div><div class=\"timeline-user\">$data[1]</div><div class=\"timeline-panel\"><div class=\"timeline-heading\"><h4 class=\"timeline-title\">$data[3]</h4><p><small class=\"text-muted\"><i class=\"glyphicon glyphicon-time\"></i>$elapsed minutes ago</small></p></div><div class=\"timeline-body\"><p>$data[4]</p></div></div></li>";
 			}else{
 			// 	$post[$i] = "
 			// 	<li clas=\"timeline-inverted\">
