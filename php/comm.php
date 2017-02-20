d2e82dae98c573624cd66fe1773693b36ae11801
Ricardo Riveron <larius828@utexas.edu>
2017-02-20 01:00:52 -0600

    ....

--- php/comm.php
+++ php/comm.php
@@ -1,22 +1,23 @@
 <?php 
+
+	define('DB_NAME','service');
+	define('DB_USER','heb');
+	define('DB_PASSWORD','Austin04');
+	define('DB_HOST', 'localhost');
+
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+	if (!$link) {
+		die('Could not connect: ' . mysql_error());
+	}
+
+	$db_selected = mysql_select_db(DB_NAME, $link);
+
+	if (!$db_selected){
+		die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+	}
+	
 	echo "<p>Im about to begin!!</p>";
-
-	// define('DB_NAME','service');
-	// define('DB_USER','heb');
-	// define('DB_PASSWORD','Austin04');
-	// define('DB_HOST', 'localhost');
-
-	// $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-
-	// if (!$link) {
-	// 	die('Could not connect: ' . mysql_error());
-	// }
-
-	// $db_selected = mysql_select_db(DB_NAME, $link);
-
-	// if (!$db_selected){
-	// 	die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
-	// }
 
 	// $result = mysql_query("SELECT * FROM comm_list");
 	// date_default_timezone_set('America/Chicago');
