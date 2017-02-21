f20c8bc942d7037cb6cbe7f6f8ae434766472a7f
Ricardo Riveron <larius828@utexas.edu>
2017-02-21 02:07:06 -0600

    ...

--- php/post_process.php
+++ php/post_process.php
@@ -6,35 +6,42 @@
 	define('DB_HOST', 'localhost');
 	session_start();
 
-	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(isset($_POST['title'],$_POST['body'],$_SESSION['username'])){
+		$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
-	if (!$link) {
-		die('Could not connect: ' . mysql_error());
+		if (!$link) {
+			die('Could not connect: ' . mysql_error());
+		}
+
+		$db_selected = mysql_select_db(DB_NAME, $link);
+
+		if (!$db_selected){
+			die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+		}
+
+		date_default_timezone_set('America/Chicago');
+		$time =date("Y-m-d h:i:sa", strtotime("now")); 
+
+		$user = $_SESSION['username'];
+		$title = $_POST['title'];
+		$body = $_POST['body'];
+
+
+
+		$col = "ID, user, time, title, body";
+		$val = "NULL, '$user', '$time', '$title', '$body'";
+
+		$sql = "INSERT INTO  comm_list ($col) VALUES ($val)";
+
+		if (!mysql_query($sql)){
+			die('Error: ' . mysql_error());
+		}
+
+		mysql_close();
+		header("Refresh:0");
+		header("Location: ../communication.php");
+	}else{
+		echo "Missing Arguments!!!"
+		header("Location: ../communication.php");
 	}
-
-	$db_selected = mysql_select_db(DB_NAME, $link);
-
-	if (!$db_selected){
-		die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
-	}
-
-	date_default_timezone_set('America/Chicago');
-	$time =date("Y-m-d h:i:sa", strtotime("now")); 
-
-	$user = $_SESSION['username'];
-	$title = $_POST['title'];
-	$body = $_POST['body'];
-
-	$col = "ID, user, time, title, body";
-	$val = "NULL, '$user', '$time', '$title', '$body'";
-
-	$sql = "INSERT INTO  comm_list ($col) VALUES ($val)";
-
-	if (!mysql_query($sql)){
-		die('Error: ' . mysql_error());
-	}
-
-	mysql_close();
-	header("Refresh:0");
-	header("Location: ../communication.php");
  ?>
